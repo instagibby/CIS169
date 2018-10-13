6a96fd465090e5250490fa2dca6802d56da3663a
Spencer Davis <spencerdavis2012@gmail.com>
2018-10-13 03:16:31 -0500

    deleted assignment zip and finished the final project for the week, and it worked as intended... in one attempt? Yea, I didn't believe it either.

---
+++ M07_Davis_TicTacToeSimulatorProgram/M07_Davis_TicTacToeSimulatorProgram/TicTacToe.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace M07_Davis_TicTacToeSimulatorProgram
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLeftLabel = new System.Windows.Forms.Label();
+            this.topCenterLabel = new System.Windows.Forms.Label();
+            this.topRightLabel = new System.Windows.Forms.Label();
+            this.centerRightLabel = new System.Windows.Forms.Label();
+            this.centerLabel = new System.Windows.Forms.Label();
+            this.centerLeftLabel = new System.Windows.Forms.Label();
+            this.bottomRightLabel = new System.Windows.Forms.Label();
+            this.bottomCenterLabel = new System.Windows.Forms.Label();
+            this.bottomLeftLabel = new System.Windows.Forms.Label();
+            this.winnerLabel = new System.Windows.Forms.Label();
+            this.NewGameBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLeftLabel
+            // 
+            this.topLeftLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLeftLabel.Location = new System.Drawing.Point(52, 27);
+            this.topLeftLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.topLeftLabel.Name = "topLeftLabel";
+            this.topLeftLabel.Size = new System.Drawing.Size(60, 68);
+            this.topLeftLabel.TabIndex = 0;
+            this.topLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topCenterLabel
+            // 
+            this.topCenterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topCenterLabel.Location = new System.Drawing.Point(135, 27);
+            this.topCenterLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.topCenterLabel.Name = "topCenterLabel";
+            this.topCenterLabel.Size = new System.Drawing.Size(60, 68);
+            this.topCenterLabel.TabIndex = 1;
+            this.topCenterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topRightLabel
+            // 
+            this.topRightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topRightLabel.Location = new System.Drawing.Point(218, 27);
+            this.topRightLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.topRightLabel.Name = "topRightLabel";
+            this.topRightLabel.Size = new System.Drawing.Size(60, 68);
+            this.topRightLabel.TabIndex = 2;
+            this.topRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // centerRightLabel
+            // 
+            this.centerRightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerRightLabel.Location = new System.Drawing.Point(218, 108);
+            this.centerRightLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.centerRightLabel.Name = "centerRightLabel";
+            this.centerRightLabel.Size = new System.Drawing.Size(60, 68);
+            this.centerRightLabel.TabIndex = 5;
+            this.centerRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // centerLabel
+            // 
+            this.centerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerLabel.Location = new System.Drawing.Point(135, 108);
+            this.centerLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.centerLabel.Name = "centerLabel";
+            this.centerLabel.Size = new System.Drawing.Size(60, 68);
+            this.centerLabel.TabIndex = 4;
+            this.centerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // centerLeftLabel
+            // 
+            this.centerLeftLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerLeftLabel.Location = new System.Drawing.Point(52, 108);
+            this.centerLeftLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.centerLeftLabel.Name = "centerLeftLabel";
+            this.centerLeftLabel.Size = new System.Drawing.Size(60, 68);
+            this.centerLeftLabel.TabIndex = 3;
+            this.centerLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomRightLabel
+            // 
+            this.bottomRightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomRightLabel.Location = new System.Drawing.Point(218, 189);
+            this.bottomRightLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.bottomRightLabel.Name = "bottomRightLabel";
+            this.bottomRightLabel.Size = new System.Drawing.Size(60, 68);
+            this.bottomRightLabel.TabIndex = 8;
+            this.bottomRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomCenterLabel
+            // 
+            this.bottomCenterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomCenterLabel.Location = new System.Drawing.Point(135, 189);
+            this.bottomCenterLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.bottomCenterLabel.Name = "bottomCenterLabel";
+            this.bottomCenterLabel.Size = new System.Drawing.Size(60, 68);
+            this.bottomCenterLabel.TabIndex = 7;
+            this.bottomCenterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomLeftLabel
+            // 
+            this.bottomLeftLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomLeftLabel.Location = new System.Drawing.Point(52, 189);
+            this.bottomLeftLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.bottomLeftLabel.Name = "bottomLeftLabel";
+            this.bottomLeftLabel.Size = new System.Drawing.Size(60, 68);
+            this.bottomLeftLabel.TabIndex = 6;
+            this.bottomLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.winnerLabel.Location = new System.Drawing.Point(52, 271);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(225, 23);
+            this.winnerLabel.TabIndex = 9;
+            this.winnerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NewGameBtn
+            // 
+            this.NewGameBtn.FlatAppearance.BorderSize = 0;
+            this.NewGameBtn.Location = new System.Drawing.Point(89, 306);
+            this.NewGameBtn.Name = "NewGameBtn";
+            this.NewGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.NewGameBtn.TabIndex = 10;
+            this.NewGameBtn.Text = "New &Game";
+            this.NewGameBtn.UseVisualStyleBackColor = true;
+            this.NewGameBtn.Click += new System.EventHandler(this.NewGameBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.FlatAppearance.BorderSize = 0;
+            this.ExitBtn.Location = new System.Drawing.Point(170, 306);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 11;
+            this.ExitBtn.Text = "E&xit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 343);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.NewGameBtn);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.bottomRightLabel);
+            this.Controls.Add(this.bottomCenterLabel);
+            this.Controls.Add(this.bottomLeftLabel);
+            this.Controls.Add(this.centerRightLabel);
+            this.Controls.Add(this.centerLabel);
+            this.Controls.Add(this.centerLeftLabel);
+            this.Controls.Add(this.topRightLabel);
+            this.Controls.Add(this.topCenterLabel);
+            this.Controls.Add(this.topLeftLabel);
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe Simulator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label topLeftLabel;
+        public System.Windows.Forms.Label topCenterLabel;
+        public System.Windows.Forms.Label topRightLabel;
+        public System.Windows.Forms.Label centerRightLabel;
+        public System.Windows.Forms.Label centerLabel;
+        public System.Windows.Forms.Label centerLeftLabel;
+        public System.Windows.Forms.Label bottomRightLabel;
+        public System.Windows.Forms.Label bottomCenterLabel;
+        public System.Windows.Forms.Label bottomLeftLabel;
+        private System.Windows.Forms.Label winnerLabel;
+        private System.Windows.Forms.Button NewGameBtn;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
+
---
+++ M07_Davis_TicTacToeSimulatorProgram/M07_Davis_TicTacToeSimulatorProgram/TicTacToe.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/** =========================================================
+
+ Spencer Davis
+ Windows 7
+ Microsoft Visual Studio 2017
+ CIS 169 C# 
+ Module 7 Tic-Tac-Toe Simulator
+ Project Description: User clicks the new game button and a 2 dimensional
+    array is created and is looped through to store a random 0 or 1 in each
+    index. Then based on those results a many if statements are ran to set
+    values for the display to be either an X (if 1) or a O (if 0). I decided
+    to then save the values from each row / column / diagnal into a single int
+    so that I can easily test if it was three in a row (three in a row has to
+    to be either a zero or 3, else it is broken) This could have been achieved
+    by comparing each individual item to see if it is the same as the one before
+    it but that seemed like a significant amount of extra effort for the same end
+    result. After those values are calculated the winner is checked and printed.
+
+ I attest that this is my original work.
+ I have not used unauthorized source code, either modified or unmodified.
+ I have not given other fellow student(s) access to my program.
+
+=========================================================== **/
+
+namespace M07_Davis_TicTacToeSimulatorProgram
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        int[,] gameBoard = new int[3,3];
+        Random rnd = new Random();
+
+        private void NewGameBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    gameBoard[i, j] = rnd.Next(2);
+                }
+            }
+
+            //      HERE SETS VALUES FOR DISPLAY
+            //  TOP ROW
+            //  LEFT
+            if (gameBoard[0,0] == 0)
+            {
+                topLeftLabel.Text = "O";
+            }
+            else
+            {
+                topLeftLabel.Text = "X";
+            }
+            //  Center
+            if (gameBoard[0, 1] == 0)
+            {
+                topCenterLabel.Text = "O";
+            }
+            else
+            {
+                topCenterLabel.Text = "X";
+            }
+            //  RIGHT
+            if (gameBoard[0, 2] == 0)
+            {
+                topRightLabel.Text = "O";
+            }
+            else
+            {
+                topRightLabel.Text = "X";
+            }
+
+            //  CENTER ROW
+            //  LEFT
+            if (gameBoard[1, 0] == 0)
+            {
+                centerLeftLabel.Text = "O";
+            }
+            else
+            {
+                centerLeftLabel.Text = "X";
+            }
+            //  CENTER
+            if (gameBoard[1, 1] == 0)
+            {
+                centerLabel.Text = "O";
+            }
+            else
+            {
+                centerLabel.Text = "X";
+            }
+            //  RIGHT
+            if (gameBoard[1, 2] == 0)
+            {
+                centerRightLabel.Text = "O";
+            }
+            else
+            {
+                centerRightLabel.Text = "X";
+            }
+
+            //  BOTTOM
+            //  LEFT
+            if (gameBoard[2, 0] == 0)
+            {
+                bottomLeftLabel.Text = "O";
+            }
+            else
+            {
+                bottomLeftLabel.Text = "X";
+            }
+            //  CENTER
+            if (gameBoard[2, 1] == 0)
+            {
+                bottomCenterLabel.Text = "O";
+            }
+            else
+            {
+                bottomCenterLabel.Text = "X";
+            }
+            //  RIGHT
+            if (gameBoard[2, 2] == 0)
+            {
+                bottomRightLabel.Text = "O";
+            }
+            else
+            {
+                bottomRightLabel.Text = "X";
+            }
+
+            //      DISPLAY VALUE ENDS
+            
+
+            //  Variables for each row total
+            int row1 = gameBoard[0,0] + gameBoard[0, 1] + gameBoard[0, 2];
+            int row2 = gameBoard[1,0] + gameBoard[1, 1] + gameBoard[1, 2];
+            int row3 = gameBoard[2,0] + gameBoard[2, 1] + gameBoard[2, 2];
+            //  Variables for each column total
+            int col1 = gameBoard[0,0] + gameBoard[1, 0] + gameBoard[2, 0];
+            int col2 = gameBoard[0,1] + gameBoard[1, 1] + gameBoard[2, 1];
+            int col3 = gameBoard[0,2] + gameBoard[1, 2] + gameBoard[2, 2];
+            //  Diagnals
+            int topLeftBottomRight = gameBoard[0, 0] + gameBoard[1, 1] + gameBoard[2, 2];
+            int topRightBottomLeft = gameBoard[0, 2] + gameBoard[1, 1] + gameBoard[2, 0];
+
+            //  Counts for *three in a row*
+            int xWinningCount = 0;
+            int oWinningCount = 0;
+
+            //  Array of each rows total values to test and count
+            int[] testCasesForPoints = { row1, row2, row3, col1, col2, col3, topRightBottomLeft, topLeftBottomRight };
+
+            //  Runs foreach to test if the row is of 0 value (O + 1 point)
+            //  if value is 3 (X + 1 point)
+            foreach (int val in testCasesForPoints)
+            {
+                if (val == 0)
+                {
+                    oWinningCount++;
+                }
+                else if (val == 3)
+                {
+                    xWinningCount++;
+                }
+            }
+
+            //  Tests win conditions to declare a winner or if it is a tie
+            if ((xWinningCount > 0 && oWinningCount > 0) || (xWinningCount == 0 && oWinningCount == 0))
+            {
+                winnerLabel.Text = "Tie Game!";
+            } else if (xWinningCount > oWinningCount)
+            {
+                winnerLabel.Text = "X Wins!";
+            } else
+            {
+                winnerLabel.Text = "O Wins!";
+            }
+        }
+        
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            //  Exits
+            this.Close();
+        }
+    }
+}
