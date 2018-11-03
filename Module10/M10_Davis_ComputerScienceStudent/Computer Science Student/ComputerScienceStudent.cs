f34e5558342e07cb07ea83ba3efceca8d39b6a8d
Spencer Davis <spencerdavis2012@gmail.com>
2018-11-03 02:02:38 -0500

    two assignments done, one to go for tomorrow

---
+++ Module10/M10_Davis_ComputerScienceStudent/Computer Science Student/ComputerScienceStudent.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/** =========================================================
+
+ Spencer Davis
+ Windows 7
+ Microsoft Visual Studio 2017
+ CIS 169 C# 
+ Module 10 Computer Science Student
+ Project Description: User can enter a student name and id number and 
+    it creates a new student, based on which checkbox is selected it displays
+    hours required. There is a reset button that resets all the values on the
+    form as well as an exit button.
+
+ I attest that this is my original work.
+ I have not used unauthorized source code, either modified or unmodified.
+ I have not given other fellow student(s) access to my program.
+
+=========================================================== **/
+
+namespace Computer_Science_Student
+{
+    public partial class ComputerScienceStudent : Form
+    {
+        public ComputerScienceStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void getHoursButton_Click(object sender, EventArgs e)
+        {
+            // Variables to hold input
+            string name, id, track;
+
+            // Get the student's name and ID.
+            name = nameTextBox.Text;
+            id = idTextBox.Text;
+
+            // Get the student's academic track.
+            if (infoSystemsRadioButton.Checked)
+            {
+                track = "Information Systems";
+            }
+            else
+            {
+                track = "Software Engineering";
+            }
+
+            // Create a CompSciStudent object.
+            CompSciStudent csStudent =
+                new CompSciStudent(name, id, track);
+
+            // Display the student's required hours.
+            requiredHoursLabel.Text = csStudent.RequiredHours.ToString("n1");
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Closes the form
+            this.Close();
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = "";
+            idTextBox.Text = "";
+            softwareEngineeringRadioButton.Checked = false;
+            infoSystemsRadioButton.Checked = false;
+            requiredHoursLabel.Text = "";
+        }
+    }
+}
---
+++ Module10/M10_Davis_Polymorphism/Polymorphism/Polymorphism.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/** =========================================================
+
+ Spencer Davis
+ Windows 7
+ Microsoft Visual Studio 2017
+ CIS 169 C# 
+ Module 10 Polymorphism
+ Project Description: User can click a button that lists either a regular
+    generic animal, or a dog / cat that are a sublcass of the animal. 
+    Based on that the user will see a text box that prints the animal's sound.
+
+ I attest that this is my original work.
+ I have not used unauthorized source code, either modified or unmodified.
+ I have not given other fellow student(s) access to my program.
+
+=========================================================== **/
+
+namespace Polymorphism
+{
+    public partial class Polymorphism : Form
+    {
+        public Polymorphism()
+        {
+            InitializeComponent();
+        }
+        
+        // The ShowAnimalInfo method accepts an Animal
+        // object as an argument. It displays the object's
+        // species and calls its MakeSound method.
+        private void ShowAnimalInfo(Animal animal)
+        {
+            MessageBox.Show("Species: " + animal.Species);
+            animal.MakeSound();
+        }
+
+        private void createAnimalButton_Click(object sender, EventArgs e)
+        {
+            Animal myAnimal = new Animal("Regular Animal");
+            ShowAnimalInfo(myAnimal);
+        }
+
+        private void createDogButton_Click(object sender, EventArgs e)
+        {
+            Dog myDog = new Dog("Fido");
+            MessageBox.Show("The dog's name is " + myDog.Name);
+            ShowAnimalInfo(myDog);
+        }
+
+        private void createCatButton_Click(object sender, EventArgs e)
+        {
+            Cat myCat = new Cat("Kitty");
+            MessageBox.Show("The cat's name is " + myCat.Name);
+            ShowAnimalInfo(myCat);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Exits form
+            this.Close();
+        }
+    }
+}
